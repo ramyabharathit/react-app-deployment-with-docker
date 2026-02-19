6b2b0067d07ca642aa9cf064487bd900aa1cb8fe
root <root@ip-172-31-1-118.ec2.internal>
2026-02-19 17:05:50 +0000

    Updated docker hub username

--- build.sh
+++ build.sh
@@ -15,4 +15,4 @@ docker run -d -it --name react -p 80:80 react-ci/cd
 
 #pushing the image to dockerhub:
 docker tag react-ci/cd rajagopal1326/react-app:ci-cd
-docker push rajagopal1326/react-app:ci-cd
+docker push ramyabharathi1804/react-app:ci-cd
